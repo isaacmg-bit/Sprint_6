d795656964fc47141abdf3e4335e760e5bb59d9c
Isaac Malagón <isaacmalagongarcia@gmail.com>
2025-12-26 20:24:12 +0100

    feat: added panel component

--- src/app/panel/panel.ts
+++ src/app/panel/panel.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Budget } from '../services/budget';
 
 @Component({
   selector: 'app-panel',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './panel.html',
   styleUrl: './panel.css',
 })
 export class Panel {
+  private readonly MIN_PAG = 1;
+  private readonly MIN_LANG = 1;
 
+  panelForm = new FormGroup({
+    pages: new FormControl(this.MIN_PAG),
+    languages: new FormControl(this.MIN_LANG),
+  });
+
+  constructor(private budgetService: Budget) {
+    this.panelForm.valueChanges.subscribe((values) => {
+      const pages = values.pages ?? this.MIN_PAG;
+      const languages = values.languages ?? this.MIN_LANG;
+
+      const price = this.budgetService.calculateWeb(pages, languages);
+
+      this.budgetService.webExtra.set(price);
+    });
+  }
+
+  increment(controlName: string) {
+    const control = this.panelForm.get(controlName);
+
+    if (control) {
+      control.setValue(control.value + 1);
+    }
+  }
+  decrement(controlName: string) {
+    const control = this.panelForm.get(controlName);
+    const minVal = controlName === 'pages' ? this.MIN_PAG : this.MIN_LANG;
+
+    if (control?.value > minVal) {
+      control?.setValue(control.value - 1);
+    } else {
+      console.warn(`El mínim és ${minVal}`);
+    }
+  }
 }
--- src/app/services/budget.ts
+++ src/app/services/budget.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Budget {
+
+
   private readonly PRICE_PER_UNIT = 30;
 
   calculateWeb(pages: number, languages: number): number {
